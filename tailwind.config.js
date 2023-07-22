65c54c216ade14dcedf806332cb59b90b5a93297
Shanto57575 <shanto57575@gmail.com>
2023-07-22 10:23:57 +0600

    allSet

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: ["light", "night", "luxury"],
+  },
+  plugins: [require("daisyui")],
+}
+
